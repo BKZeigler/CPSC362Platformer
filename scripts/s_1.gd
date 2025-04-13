603504cab76cb5d8ef1ce0c0dadc4da19d73398f
Brian Milian <brianmilian@csu.fullerton.edu>
2025-04-13 01:42:46 -0700

    Update pop-up-message variable name

--- scripts/s_1.gd
+++ scripts/s_1.gd
@@ -7,7 +7,7 @@ extends Node2D
 @onready var green: Button = $ColorRect/green
 @onready var click_count_label: Label = $"ColorRect/click count"
 @onready var timer_label: Label = $ColorRect/timer
-@onready var pop_up_message: Label = $"ColorRect/pop up message"
+@onready var popup_label: Label = $"ColorRect/pop up message"
 
 var statements = [
 	"Click Red",
