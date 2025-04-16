f61d2bc0333977e071656c39a1f822525b06e322
Lorenzo Visitacion <lorenzjvisitacion10@gmail.com>
2025-04-16 16:11:18 -0700

    Added cipher for level 1

---
+++ scenes/level_1c.gd
@@ -0,0 +1,43 @@
+extends CanvasLayer
+
+@onready var line_edit = $inputbox/LineEdit
+@onready var next_level = $nextLevel
+@onready var fail_msg = $bg1/bg2/correctMsg
+@onready var success_msg = $bg1/bg2/correctMsg
+@onready var bg_rect = $bg1
+@onready var submit_button = $submit
+
+
+var password = "test"
+
+func _ready():
+	print("code is running")
+	line_edit.grab_focus()
+	next_level.visible = false
+	fail_msg.visible = false
+	success_msg.visible = false
+
+func _check_password():
+	print("check password function called")
+	var user_input = line_edit.text
+	if user_input == password:
+		print("Success")
+		fail_msg.visible = false
+		success_msg.visible = true
+		submit_button.visible = false
+		next_level.visible = true
+		bg_rect.set_color(Color(0,255,0))
+	else:
+		bg_rect.set_color(Color(255,0,0))
+		fail_msg.visible = true
+		print("Fail")
+		
+
+func _on_submit_pressed() -> void:
+	print("submit button pressed")
+	_check_password()
+	
+
+func _on_next_level_pressed() -> void:
+	print("next level button pressed")
+	#idk what to do here
